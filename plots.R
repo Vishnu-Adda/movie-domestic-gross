b18ca3478381fce4c87aea1cd090367320c7e703
Vishnu Adda <addavishnu@gmail.com>
2018-06-27 01:53:31 -0700

    Commit final visualization.

--- plots.R
+++ plots.R
@@ -33,6 +33,47 @@ filt2
 mov2 <- mov[filt & filt2,]
 mov2
 
+#prepare the plot's data and aes layers:
+p <- ggplot(data=mov2, aes(x=Genre, y=Gross...US))
+p
+
+#add geometries
+q <- p + geom_jitter(aes(size=Budget...mill., color=Studio)) + 
+  geom_boxplot(alpha=0.7, outlier.color=NA)
+q
+
+#non-data ink
+q <- q + xlab("Genre") +
+  ylab("Gross % US") +
+  ggtitle("Domestic Gross % by Genre")
+q
+
+#theme
+q <- q + 
+  theme(
+    axis.title.x = element_text(color="Blue", size=30),
+    axis.title.y = element_text(color="Blue", size=30),
+    axis.text.x = element_text(size=20),
+    axis.text.y = element_text(size=20),
+    
+    plot.title = element_text(size=40),
+    
+    legend.title = element_text(size=20),
+    legend.text = element_text(size=20)
+    
+  )
+
+q
+
+# Final touch
+q$labels$size <- "Budget $M"
+
+
+
+
+
+
+
 
 
 
